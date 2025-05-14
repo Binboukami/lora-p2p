f56229d4d33d258421d44b3b413cf7a8245e3792
Binboukami <luisalexsh@gmail.com>
2025-05-14 13:15:31 -0300

    add example implementation for uart packet processing [WIP]

--- src/broker.h
+++ src/broker.h
@@ -12,38 +12,96 @@ Packet pacote;
 
 void _setup() {
 
-  #ifdef DEBUG_SERIAL
-    UART0.begin(115200);
-  #endif
+  // #ifdef DEBUG_SERIAL
+  UART0.begin(115200);
+  // #endif
 
-  LoRa.onTxDone(onTxDone);
-  LoRa.onReceive(onReceive);
+  // LoRa.onTxDone(onTxDone);
+  // LoRa.onReceive(onReceive);
 
-  LoRa.receive(); // Puts the radio in continuous receive mode.
+  // LoRa.receive(); // Puts the radio in continuous receive mode.
 }
 
+#define UART_BUFF_SZ 32
+
+char *uart_buff_in[UART_BUFF_SZ];
+char *uart_buff_out[UART_BUFF_SZ];
+
+// 127 bytes
+struct UART_PKT {
+  struct UART_PKT_HEADER {
+    // unsigned int _STX_;          // 1 8bits
+    char _STX_A;
+    char _STX_B;
+    unsigned short _ID_;            // 2 16bits
+    unsigned short _LEN_;           // 2 16bits
+  } _HEAD_;
+  char _DATA_[121];                 // 121 bytes
+  char _C_PAD_;
+  char _ETX_;                       // 1
+};
+
 void _loop() {
-  if(onInterval())
+  if(onInterval(1000))
   {
-    // Sends messages to single/multiple clients
-    out_p.op_code = 0x03;
-    strcpy(out_p.payload, "Liga motor\0");
+    // TODO: Process messages incoming from serial port
+    // TODO: Show messages on display (for debugging)
+    // Server Application -> UART -> Process -> LoRa packet
+    while(UART0.available())
+    {
+      // 3-Step operation
+      // 1: Read everything into a binary buffer
+      UART0.readBytesUntil('\n', (char*)&uart_buff_in, sizeof(uart_buff_in));
+      // 2: Parse header to get hints on how to parse the packet body
+      //    - Read until heaader delimiter (define later) something like \h\h
+      // 3: Parse packet body
+      //    - Read remaining buffer content up to specified size casted by packet
+      //    parse_packet((packer*)buffer);
+    }
 
-    // enviando 0x03
-    LoRa.beginPacket();
-    LoRa.write(out_p.op_code);
-    LoRa.printf("%s\0", out_p.payload);
-    LoRa.endPacket(true);
+    // Forward call to remote ESP32 stub
+    /*
+      switch(command):
+        case ???: call run_cmd_a();
+      ...
 
-    #ifdef HAS_DISPLAY
-      display.setCursor(0, 0);
-      display.println("RECEBIDO: ");
-      display.printf("OP_CODE: %u \n", pacote.op_code);
-      display.printf("PAYLOAD: %s \n", pacote.payload);
+      LoRa.send(call_header, call_payload)
+    */
 
-      display.println("ENVIANDO: ");
-      display.printf("OP_CODE: %u \n", out_p.op_code);
-      display.printf("PAYLOAD: %s \n", out_p.payload);
+    // TODO: Process incoming RPC calls through LoRa interface and forward to server application
+    // LoRa packet -> Process -> UART -> Server Application 
+    // Simulate LoRa receive callback
+    {
+      // Forward messages stored in a queue
+      // Hint: The actual packet can be reused, the value that change are
+      //       id, content_length, and content
+
+      UART_PKT test_packet = {
+        ._HEAD_ = {
+          // ._STX_ = 0xFFAA,
+          ._STX_A = 0xFF,
+          ._STX_B = 0xAA,
+          ._ID_ = 0x01,
+          ._LEN_ = 121
+        },
+        ._C_PAD_ = 'Z',
+        ._ETX_ = '\n'
+      };
+
+      strcpy(test_packet._DATA_, "Hello, World");
+
+      memcpy((char*)&uart_buff_out, &test_packet, sizeof(test_packet));
+
+      if(UART0.availableForWrite())
+      {
+        UART0.write((char*)&uart_buff_out, sizeof(uart_buff_out));
+      }
+    }
+
+    #ifdef USE_DISPLAY
+      display.clear();
+      display.printf("Waiting...\n");
+      display.printf("R: %s\n", uart_buff_in);
     #endif
   }
 }
--- src/common/packet.h
+++ src/common/packet.h
@@ -1,8 +1,76 @@
 #pragma once
 
-#define MAX_PAYLOAD 127
+#define PACKET_MAX_PAYLOAD 127
+#define PACKET_QUEUE_MAX_SZ 2
+#include <queue>
 
 struct Packet {
   char op_code;
-  char payload[MAX_PAYLOAD];
+  char payload[PACKET_MAX_PAYLOAD];
+};
+
+/** TODO: Debug assert the packet size, should be ~127 bytes */
+struct UART_PACKET {
+  struct UART_PACKET_HEADER {
+    char _SYNCTX0_;                 // 1 byte
+    char _SYNCTX1_;                 // 1 byte
+    unsigned short _ID_;            // 2 bytes 16bits
+    unsigned short _LEN_;           // 2 bytes 16bits
+  } _HEAD_;
+  char _DATA_[PACKET_MAX_PAYLOAD];                 // 121 bytes
+  char _C_PAD_;                     // 1 byte
+  char _ETX_;                       // 1 byte
+};
+
+struct UART_PACKET_MODIFIABLE_CONTENT {
+  unsigned short _ID_;
+  unsigned short _LEN_;
+  char* _DATA_;
+};
+
+struct PacketQueue {
+  UART_PACKET _proto_uart_packet;
+
+  size_t _packets_qeueue_max_sz = PACKET_QUEUE_MAX_SZ;
+  std::queue<UART_PACKET_MODIFIABLE_CONTENT> _packets;
+
+  /** Returns true if there are packets in the queue */
+  inline bool isWaiting() { return (!_packets.empty()); };
+
+  bool push(unsigned short id, unsigned short data_len, char* data)
+  {
+    size_t current_size = _packets.size();
+
+    if(current_size == _packets_qeueue_max_sz)
+      return 1;
+
+    _packets.push(
+      (UART_PACKET_MODIFIABLE_CONTENT){ ._ID_ = id, ._LEN_ = data_len, ._DATA_ = data });
+
+    // Pushed sucessfully if 0
+    return (_packets.size() > current_size);
+  }
+
+  void mountNext()
+  {
+    UART_PACKET_MODIFIABLE_CONTENT next = _packets.back();
+
+    _proto_uart_packet._HEAD_._ID_ = next._ID_;
+    _proto_uart_packet._HEAD_._LEN_ = next._LEN_;
+
+    strcpy(_proto_uart_packet._DATA_, next._DATA_);
+
+    _packets.pop();
+  }
+
+  /** Consumes the internal proto_packet */
+  inline void pipe(char* output_buffer)
+  {
+    memcpy((char*)&output_buffer, &_proto_uart_packet, sizeof(_proto_uart_packet));
+
+    // clean-up
+    _proto_uart_packet._HEAD_._ID_ = 0;
+    _proto_uart_packet._HEAD_._LEN_ = 0;
+    memset(_proto_uart_packet._DATA_, 0, sizeof(char)*PACKET_MAX_PAYLOAD);
+  }
 };
